163402bc373c284d000a81287fa682e4057602fd
Jose Ramirez <shadow661199@gmail.com>
2018-06-01 16:33:43 -0700

    recipe book project

---
+++ header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector:'header-lmnt',
+    templateUrl:'./header.component.html',
+    styles:['./header.component.css']
+})
+
+export class HeaderComponent{
+
+
+}
+
